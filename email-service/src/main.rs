50f708b5acd5b9a9ffd236ba4ae39b90700dfbc5
Johannes Sluis <joesluis51@gmail.com>
2025-11-23 15:46:46 -0800

    sending email based on messages in queue

--- email-service/src/main.rs
+++ email-service/src/main.rs
@@ -9,7 +9,38 @@ use azure_messaging_servicebus::prelude::*;
 use azure_core::Result;
 use std::sync::Arc;
 
-async fn azure_creds_init() -> Result<()> {
+fn create_mailer() -> SmtpTransport {
+    let username = std::env::var("EMAIL_USERNAME").expect("EMAIL_USERNAME not set");
+    let password = std::env::var("EMAIL_PASSWORD").expect("EMAIL_PASSWORD not set");
+    println!("user name is {username}");
+
+    let creds = Credentials::new(username, password);
+
+    SmtpTransport::relay("smtp.gmail.com")
+        .unwrap()
+        .credentials(creds)
+        .build()
+}
+
+fn send_email(body: String) {
+    let email = Message::builder()
+        .from("joesluis51@gmail.com".parse::<Mailbox>().unwrap())
+        .to("joesluis51@gmail.com".parse::<Mailbox>().unwrap())
+        .subject("email with rust")
+        .body(body)
+        .unwrap();
+
+    let mailer = create_mailer();
+
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent!"),
+        Err(error) => {
+            println!("Email didn't send. {:?}", error);
+        }
+    }
+}
+
+async fn get_message_and_send_email() -> Result<()> {
      let http_client: Arc<dyn azure_core::HttpClient> = Arc::new(reqwest::Client::new());
      let service_bus_namespace = std::env::var("SERVICE_BUS_NAMESPACE").expect("SERVICE_BUS_NAMESPACE not set");
      let queue_name = std::env::var("QUEUE_NAME").expect("QUEUE_NAME not set");
@@ -28,50 +59,22 @@ async fn azure_creds_init() -> Result<()> {
 
     let msg = client.receive_and_delete_message().await?;
 
-    println!("{msg}");
+    println!("received message {msg}");
+
+    create_mailer();
+
+    println!("Sending email...");
+
+    send_email(msg);
     
     Ok(())
-}
-
-fn create_mailer() -> SmtpTransport {
-    let username = std::env::var("EMAIL_USERNAME").expect("EMAIL_USERNAME not set");
-    let password = std::env::var("EMAIL_PASSWORD").expect("EMAIL_PASSWORD not set");
-    println!("user name is {username}");
-
-    let creds = Credentials::new(username, password);
-
-    SmtpTransport::relay("smtp.gmail.com")
-        .unwrap()
-        .credentials(creds)
-        .build()
-}
-
-fn send_email() {
-    let email = Message::builder()
-        .from("joesluis51@gmail.com".parse::<Mailbox>().unwrap())
-        .to("joesluis51@gmail.com".parse::<Mailbox>().unwrap())
-        .subject("email with rust")
-        .body("sending this email using rust congrats broski".to_string())
-        .unwrap();
-
-    let mailer = create_mailer();
-
-    match mailer.send(&email) {
-        Ok(_) => println!("Email sent!"),
-        Err(error) => {
-            println!("Email didn't send. {:?}", error);
-        }
-    }
 }
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    azure_creds_init().await.unwrap_or_else(|e| {
+    get_message_and_send_email().await.unwrap_or_else(|e| {
         eprintln!("Error initializing Azure credentials: {}", e);
     });
 
-    //println!("Sending email...");
-    //
-    //send_email();
 }
